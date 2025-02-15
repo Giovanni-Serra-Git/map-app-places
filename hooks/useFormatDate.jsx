105ca6b64f975f7fd8b3edf6b3da747adf3a5c7e
Giovanni-Serra-Git <giovanni.serra.web@gmail.com>
2025-02-15 12:28:24 +0100

    first commit

--- hooks/useFormatDate.jsx
+++ hooks/useFormatDate.jsx
@@ -2,14 +2,17 @@ import { useMemo } from "react"
 ;
 
 function useFormatDate(date) {
-    console.log(date)
-    return
     return useMemo(() => {
+        if (!date || isNaN(new Date(date).getTime())) {
+            return "Invalid Date";
+        }
+
         return new Intl.DateTimeFormat("en", {
             day: "numeric",
             month: "long",
             year: "numeric",
-          }).format(new Date(date));
+            timeZone: "UTC",  // Forza l'uso di UTC (opzionale)
+        }).format(new Date(date));
     }, [date])
 }
 
