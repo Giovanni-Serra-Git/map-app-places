4a725095d2f4873e7f1d8a5f0325bf0f24d9274d
Giovanni-Serra-Git <giovanni.serra.web@gmail.com>
2025-02-15 12:23:25 +0100

    first commit

--- components/City.jsx
+++ components/City.jsx
@@ -12,6 +12,7 @@ function City({city}) {
 
     console.log("Inside City")
     console.log(city)
+    console.log(city.date)
 
     const {cities, currentCity, error} = useCities()
 
@@ -29,7 +30,7 @@ function City({city}) {
                     <p>{city.cityName}</p>
               </div>
               <div className="flex items-center gap-[6px]">
-                  <div>{formattingDate(city.date)}</div>
+                  <div>{formattingDate(city.date) || ""}</div>
                   <button onClick={(e) => dispatch(deleteCity(e,city.id, city, cities))} className="cursor-pointer"><p className="text-lg">&times;</p></button>
               </div>
           </div>
